89f1b8ff0070f52891f06559345a3cfcf7a0023c
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-08-26 02:07:31 +0500

    feat: api key changed

--- src/services/token.ts
+++ src/services/token.ts
@@ -4,7 +4,7 @@ export const getAccessToken = () => {
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
-      'x-api-key': process.env.NEXT_PUBLIC_HEYGEN_API_KEY || 'MGExNmVhZWFlNmYzNGIzY2FlMjZiOGU0ZTY5MGZkZjUtMTc1NjEzOTc4MQ==',
+      'x-api-key': process.env.NEXT_PUBLIC_HEYGEN_API_KEY || 'NWFhYzQ0ZWM5MDlmNDhjNTg5MjU1YWI1YTJhYmFkMWYtMTc1NjE1NTgzMg==',
     },
     body: JSON.stringify({})
   };
